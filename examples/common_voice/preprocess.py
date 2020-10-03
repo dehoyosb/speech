9d56dcb80ef4af3455a8016d1d187b1a05e267a1
dehoyosb <daniel.hoyos@mercadolibre.com.co>
2020-10-03 01:56:44 -0500

    updated complete audio preprocessing pipe line with common voice data and a phonemizer to go from text to phonems, which is needed for the ctc training

--- examples/common_voice/preprocess.py
+++ examples/common_voice/preprocess.py
@@ -8,17 +8,17 @@ import glob
 import json
 import os
 import random
-import tqdm
+import pandas as pd
+
+from tqdm import tqdm
 
 from speech.utils import data_helpers
 from speech.utils import wave
 
-WAV_EXT = "wv" # using wv since NIST took wav
-TEST_SPEAKERS = [ # Core test set from timit/readme.doc
-    'mdab0', 'mwbt0', 'felc0', 'mtas1', 'mwew0', 'fpas0',
-    'mjmp0', 'mlnt0', 'fpkt0', 'mlll0', 'mtls0', 'fjlm0',
-    'mbpm0', 'mklt0', 'fnlp0', 'mcmj0', 'mjdh0', 'fmgd0',
-    'mgrt0', 'mnjm0', 'fdhc0', 'mjln0', 'mpam0', 'fmld0']
+from phonemizer import phonemize
+from phonemizer.separator import default_separator, Separator
+
+WAV_EXT = "wav" # using wv since NIST took wav
 
 def load_phone_map():
     with open("phones.60-48-39.map", 'r') as fid:
@@ -28,21 +28,18 @@ def load_phone_map():
     m48_39 = {l[1] : l[2] for l in lines}
     return m60_48, m48_39
 
-def load_transcripts(path):
-    pattern = os.path.join(path, "*/*/*.phn")
+def load_transcripts(path: str, split:str, use_percentage: float) -> dict:
+    dataset = pd.read_csv(os.path.join(path, "{}.tsv".format(split)), sep='\t')
+    length = len(dataset)
     m60_48, _ = load_phone_map()
-    files = glob.glob(pattern)
-    # Standard practic is to remove all "sa" sentences
-    # for each speaker since they are the same for all.
-    filt_sa = lambda x : os.path.basename(x)[:2] != "sa"
-    files = filter(filt_sa, files)
     data = {}
-    for f in files:
-        with open(f) as fid:
-            lines = (l.strip() for l in fid)
-            phonemes = (l.split()[-1] for l in lines)
-            phonemes = [m60_48[p] for p in phonemes if p in m60_48]
-            data[f] = phonemes
+    for file_name, text in tqdm(dataset[['path','sentence']].values[:int(use_percentage*(length))]):
+        phn = phonemize(text = text, 
+                        language='en-us',
+                        backend='festival',
+                        separator=phone_separator).split()
+        phonemes = [m60_48[p] for p in phn if p in m60_48]
+        data[os.path.join(path,'clips',file_name)] = phonemes
     return data
 
 def split_by_speaker(data, dev_speakers=50):
@@ -63,14 +60,14 @@ def split_by_speaker(data, dev_speakers=50):
     return dev, test
 
 def convert_to_wav(path):
-    data_helpers.convert_full_set(path, "*/*/*/*.wav",
-            new_ext=WAV_EXT,
-            use_avconv=False)
+    data_helpers.convert_full_set(path, "*.mp3",
+                                  new_ext=WAV_EXT,
+                                  use_avconv=False)
 
 def build_json(data, path, set_name):
     basename = set_name + os.path.extsep + "json"
     with open(os.path.join(path, basename), 'w') as fid:
-        for k, t in tqdm.tqdm(data.items()):
+        for k, t in tqdm(data.items()):
             wave_file = os.path.splitext(k)[0] + os.path.extsep + WAV_EXT
             dur = wave.wav_duration(wave_file)
             datum = {'text' : t,
@@ -81,26 +78,40 @@ def build_json(data, path, set_name):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-            description="Preprocess Timit dataset.")
-
+            description="Preprocess Common Voice dataset.")
     parser.add_argument("output_directory",
-        help="Path where the dataset is saved.")
+        help="Path where the dataset is saved.",
+        #type = str
+        )
+    parser.add_argument("--use_percentage",
+        help="Data percentage to use.",
+        type = float,
+        default=1.0
+        )
     args = parser.parse_args()
 
-    path = os.path.join(args.output_directory, "timit")
-    path = os.path.abspath(path)
+    #TODO: implement data split by speaker
+    #TODO: implement run assuming full dataset in folder and take a portion of it
 
-    print("Converting files from NIST to standard wave format...")
-    convert_to_wav(path)
+    phone_separator = Separator(phone=' ', syllable='', word='')
 
-    print("Preprocessing train")
-    train = load_transcripts(os.path.join(path, "train"))
+    path = os.path.abspath(args.output_directory)
+
+    print("Converting files from mp3 to standard wave format...")
+    #convert_to_wav(os.path.join(path, "clips"))
+
+    print("Preprocessing train --> generating phonems from transcripts")
+    train = load_transcripts(path, "train", args.use_percentage)
+    print("Preprocessing train --> generating training jsons")
     build_json(train, path, "train")
 
-    print("Preprocessing dev")
-    transcripts = load_transcripts(os.path.join(path, "test"))
-    dev, test = split_by_speaker(transcripts)
+    print("Preprocessing dev --> generating phonems from transcripts")
+    dev = load_transcripts(path, "dev", args.use_percentage)
+    print("Preprocessing dev --> generating training jsons")
+    #dev, test = split_by_speaker(transcripts)
     build_json(dev, path, "dev")
 
-    print("Preprocessing test")
+    print("Preprocessing test --> generating phonems from transcripts")
+    test = load_transcripts(path, "test", args.use_percentage)
+    print("Preprocessing test --> generating training jsons")
     build_json(test, path, "test")
