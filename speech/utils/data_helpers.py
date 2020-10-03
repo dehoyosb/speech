778e3574240a7d0d8dd355e7dc881a480c9ac84d
Daniel <dannyehb92@gmail.com>
2020-10-03 01:58:02 -0500

    updated helper functions for the preprocessing

--- speech/utils/data_helpers.py
+++ speech/utils/data_helpers.py
@@ -4,17 +4,18 @@ from __future__ import print_function
 
 import glob
 import os
-import tqdm
+from tqdm import tqdm
 
 from speech.utils import convert
 
 def convert_full_set(path, pattern, new_ext="wav", **kwargs):
     pattern = os.path.join(path, pattern)
     audio_files = glob.glob(pattern)
-    for af in tqdm.tqdm(audio_files):
+    for af in tqdm(audio_files):
         base, ext = os.path.splitext(af)
         wav = base + os.path.extsep + new_ext
-        convert.to_wave(af, wav, **kwargs)
+        if wav not in os.listdir(path):
+            convert.to_wave(af, wav, **kwargs)
 
 
 
