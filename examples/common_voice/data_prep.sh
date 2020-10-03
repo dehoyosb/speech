ddbf8d469e1f577d4ec512b1df5b94db9f0b52e2
dehoyosb <daniel.hoyos@mercadolibre.com.co>
2020-10-03 01:58:02 -0500

    updated helper functions for the preprocessing

--- examples/common_voice/data_prep.sh
+++ examples/common_voice/data_prep.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 common_voice_path=$1
-python preprocess.py $common_voice_path
-ln -s $common_voice_path data
+usage_percentage=$2
+python preprocess.py $common_voice_path $usage_percentage
+#ln -s $common_voice_path data
